84134580392298ad940e6b47000f65da31b64ec1
Talipov S.N. <proffix4@gmail.com>
2025-02-03 12:24:32 +0500

    update 2025

--- src/kz/tsn/hibernate/TSN_HIBERNATE.java
+++ src/kz/tsn/hibernate/TSN_HIBERNATE.java
@@ -6,68 +6,89 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class TSN_HIBERNATE {
 
     public static void main(String[] args) {
+        // Открытие сессии Hibernate
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
 
-        Session session = HibernateUtil.getSessionFactory().openSession();
+            // Создание CriteriaBuilder для построения запросов
+            CriteriaBuilder cb = session.getCriteriaBuilder();
 
-        CriteriaBuilder cb = session.getCriteriaBuilder();
+            // Создание запроса для UserRole с фильтрацией по имени
+            CriteriaQuery<UserRole> criteriaUserRole = cb.createQuery(UserRole.class);
+            Root<UserRole> rootUserRole = criteriaUserRole.from(UserRole.class);
+            criteriaUserRole.select(rootUserRole).where(cb.like(rootUserRole.get("name"), "%adm%"));
 
-        CriteriaQuery<UserRole> criteriaUserRole = cb.createQuery(UserRole.class);
-        Root<UserRole> rootUserRole = criteriaUserRole.from(UserRole.class);
-        //criteriaUserRole.select(rootUserRole);
-        criteriaUserRole.select(rootUserRole).where(cb.like(rootUserRole.get("name"), "%adm%"));
+            // Создание запроса для User с несколькими условиями фильтрации
+            CriteriaQuery<User> criteriaUser = cb.createQuery(User.class);
+            Root<User> rootUser = criteriaUser.from(User.class);
+            Predicate[] predicates = new Predicate[3];
+            predicates[0] = cb.like(rootUser.get("login"), "%Test%");
+            predicates[1] = cb.like(rootUser.get("description"), "%12%");
+            predicates[2] = cb.like(rootUser.get("userRole").get("name"), "%adm%");
+            criteriaUser.select(rootUser).where(predicates);
 
-        CriteriaQuery<User> criteriaUser = cb.createQuery(User.class);
-        Root<User> rootUser = criteriaUser.from(User.class);
-        Predicate[] predicates = new Predicate[3];
-        predicates[0] = cb.like(rootUser.get("login"), "%Test%");
-        predicates[1] = cb.like(rootUser.get("description"), "%12%");
-        predicates[2] = cb.like(rootUser.get("userRole").get("name"), "%adm%");
-        criteriaUser.select(rootUser).where(predicates);
+            // Начало транзакции
+            Transaction tr = session.beginTransaction();
 
-        UserRole userRole1 = new UserRole();
-        userRole1.setName("R114");
-        userRole1.setAccessCodes("125");
-        session.save(userRole1);
+            // Создание и сохранение UserRole
+            UserRole userRole1 = new UserRole();
+            userRole1.setName("R114");
+            userRole1.setAccessCodes("125");
+            session.save(userRole1);
 
-        UserRole userRole2 = new UserRole();
-        userRole2.setName("admin");
-        userRole2.setAccessCodes("77,12144");
-        session.save(userRole2);
+            UserRole userRole2 = new UserRole();
+            userRole2.setName("admin");
+            userRole2.setAccessCodes("77,12144");
+            session.save(userRole2);
 
-        org.hibernate.Transaction tr = session.beginTransaction();
-        session.delete(userRole1);
-//        session.delete(userRole2);
-        tr.commit();
+            // Удаление ранее созданного UserRole
+            session.delete(userRole1);
+            // session.delete(userRole2); // Закомментировано, чтобы не удалять userRole2
 
-        User user = new User();
-        user.setAvailableDepartments("1,2,3,4");
-        user.setDescription("Test12");
-        user.setLogin("Test1");
-        user.setPassword("1234");
-        user.setUserRole(new UserRole("hibernate2", "7,7,7"));
-        session.save(user);
+            // Фиксация транзакции
+            tr.commit();
 
-        user = (User) session.get(User.class, user.getId());
-        user.setAvailableDepartments("777");
-        session.save(user);
+            // Начало новой транзакции для операций с User
+            Transaction trUser = session.beginTransaction();
 
-        List<User> resultsUser = session.createQuery(criteriaUser).getResultList();
-        resultsUser.forEach((item) -> {
-            System.out.println(item);
-        });
+            // Создание и сохранение User
+            User user = new User();
+            user.setAvailableDepartments("1,2,3,4");
+            user.setDescription("Test12");
+            user.setLogin("Test1");
+            user.setPassword("1234");
+            user.setUserRole(new UserRole("hibernate2", "7,7,7"));
+            session.save(user);
 
-        List<UserRole> resultsUserRole = session.createQuery(criteriaUserRole).getResultList();
-        resultsUserRole.forEach((item) -> {
-            System.out.println(item);
-        });
+            // Получение и обновление User
+            user = (User) session.get(User.class, user.getId());
+            user.setAvailableDepartments("777");
+            session.save(user);
 
-        session.close();
+            // Фиксация транзакции
+            trUser.commit();
 
+            // Выполнение запросов и вывод результатов
+            List<User> resultsUser = session.createQuery(criteriaUser).getResultList();
+            resultsUser.forEach((item) -> {
+                System.out.println(item);
+            });
+
+            List<UserRole> resultsUserRole = session.createQuery(criteriaUserRole).getResultList();
+            resultsUserRole.forEach((item) -> {
+                System.out.println(item);
+            });
+        } catch (Exception e) {
+            // Обработка исключений при работе с базой данных
+            System.err.println("Error accessing database!");
+            e.printStackTrace();
+        }
+
+        // Завершение работы приложения
         System.exit(0);
     }
-
 }
--- src/kz/tsn/hibernate/TSN_HIBERNATE_OLD.java
+++ src/kz/tsn/hibernate/TSN_HIBERNATE_OLD.java
@@ -2,58 +2,82 @@ package kz.tsn.hibernate;
 
 import java.util.List;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class TSN_HIBERNATE_OLD {
 
     public static void main(String[] args) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        // Открытие сессии Hibernate
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            
+            // Начало транзакции
+            Transaction transaction = session.beginTransaction();
 
-        UserRole userRole1 = new UserRole();
-        userRole1.setName("Boss12");
-        userRole1.setAccessCodes("736");
-        session.save(userRole1);
+            // Создание и сохранение UserRole
+            UserRole userRole1 = new UserRole();
+            userRole1.setName("Boss12");
+            userRole1.setAccessCodes("736");
+            session.save(userRole1);
 
-        UserRole userRole = new UserRole();
-        userRole.setName("TSN");
-        userRole.setAccessCodes("112144,736");
-        session.save(userRole);
+            UserRole userRole2 = new UserRole();
+            userRole2.setName("TSN");
+            userRole2.setAccessCodes("112144,736");
+            session.save(userRole2);
 
-        org.hibernate.Transaction tr = session.beginTransaction();
-        session.delete(userRole1);
-        tr.commit();
+            // Удаление ранее созданного UserRole (userRole1)
+            session.delete(userRole1);
 
-        List<UserRole> list = (List<UserRole>) session.createCriteria(UserRole.class).list();
-//	    List<UserRole> list=(List<UserRole>)session.createSQLQuery("select * from UserRole").addEntity(UserRole.class).list();
-//        List<UserRole> list = (List<UserRole>) session.createQuery("from UserRole where id in (7,10,48)").list();
-        for (UserRole role : list) {
-            System.out.println(role);
+            // Фиксация транзакции
+            transaction.commit();
+
+            // Выборка всех UserRole
+            List<UserRole> list = session.createCriteria(UserRole.class).list();
+            // Альтернативные способы выборки:
+            // List<UserRole> list = session.createSQLQuery("select * from UserRole").addEntity(UserRole.class).list();
+            // List<UserRole> list = session.createQuery("from UserRole where id in (7,10,48)").list();
+            
+            // Печать всех UserRole
+            for (UserRole role : list) {
+                System.out.println(role);
+            }
+
+            // Начало новой транзакции для операций с User
+            transaction = session.beginTransaction();
+
+            // Создание и сохранение User
+            User user = new User();
+            user.setAvailableDepartments("1,2,3,4");
+            user.setDescription("Test12");
+            user.setLogin("Test12");
+            user.setPassword("1234");
+            user.setUserRole(new UserRole("hibernate2", "7,7,7"));
+            session.save(user);
+
+            // Получение и обновление User
+            user = (User) session.get(User.class, user.getId());
+            user.setAvailableDepartments("777");
+            session.save(user);
+
+            // Фиксация транзакции
+            transaction.commit();
+
+            // Выборка всех User
+            List<User> list2 = session.createCriteria(User.class).list();
+            // Альтернативные способы выборки с сортировкой и фильтрацией:
+            // List<User> list2 = session.createCriteria(User.class).addOrder(Order.asc("login")).createCriteria("userRole").add(Expression.like("name", "%")).addOrder(Order.asc("name")).list();
+
+            // Печать всех User
+            list2.forEach((item) -> {
+                System.out.println(item);
+            });
+
+        } catch (Exception e) {
+            // Обработка исключений при работе с базой данных
+            System.err.println("Error accessing database!");
+            e.printStackTrace();
         }
 
-        User user = new User();
-        user.setAvailableDepartments("1,2,3,4");
-        user.setDescription("Test12");
-        user.setLogin("Test12");
-        user.setPassword("1234");
-        user.setUserRole(new UserRole("hibernate2", "7,7,7"));
-        session.save(user);
-
-        user = (User) session.get(User.class, user.getId());
-        user.setAvailableDepartments("777");
-        session.save(user);
-
-        List<User> list2 = (List<User>) session
-                .createCriteria(User.class)
-                //  .addOrder(Order.asc("login"))
-                // .createCriteria("userRole")
-                //.add(Expression.like("name", "%"))
-                //.addOrder(Order.asc("name"))
-                .list();
-        list2.forEach((item) -> {
-            System.out.println(item);
-        });
-
-        session.close();
-
+        // Завершение работы приложения
         System.exit(0);
     }
 }
